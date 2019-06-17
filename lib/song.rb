c41a7c436020ec3d1770052dbdc38a0016d3f236
Kevin O'Brien <kpobrien@buffalo.edu>
2019-06-17 10:48:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,45 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song
+  end
+
+  def self.new_by_name
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) == nil
+        self.create_by_name(name)
+      else
+        self.find_by_name(name)
+      end
+  end
+
+  def self.alphabetical
+    @@all.sort_by{|x| x.name}
+  end
+
+
+
+  def self.destroy_all
+    @@all.clear
+  end
+
 end
